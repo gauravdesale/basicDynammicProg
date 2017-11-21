93d9e7d753521759a14523ce8d02a8929e90888f
gauravdesale <desalegaurav60@gmail.com>
2017-11-20 17:37:24 -0800

    adding more practice

---
+++ fibMemoiz.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func checkTable(a int) int {
+	var lookUp [100]int
+	var indexLookUp [100]int
+	for i := 0; i < 100; i++ {
+		if indexLookUp[i] == a {
+			return lookUp[i]
+		} 
+		
+	}
+	return 0
+}
+
+func addToArrays(newIndex int, newValue int) int {
+
+}
